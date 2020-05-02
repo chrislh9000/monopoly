d77c07161573d168c16922ebf9dfeb20050fe2aa
Christopher Lee Hemm <chemm@Christophers-MacBook-Pro-3.local>
2020-05-02 00:28:47 -0500

    added other  pages

--- src/components/Bank.jsx
+++ src/components/Bank.jsx
@@ -21,7 +21,7 @@ class Bank extends React.Component {
   render() {
     return (
         <div>
-        <h1> Main!!! </h1>
+        <h1> BANK!!! </h1>
         </div>
       );
     }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl'
 import Input from '@material-ui/core/Input'
+import Bank from './Bank.jsx'
 
 class Main extends React.Component {
   constructor(props) {
@@ -11,7 +12,9 @@ class Main extends React.Component {
     this.state = {
       gameState: {
         players:['Chris',  'Arjun', 'Noah'],
+        turn: 1,
       },
+      changeBank: false,
     }
   }
 
@@ -20,10 +23,37 @@ class Main extends React.Component {
       name: e.target.value
     })
   }
+
+  changeBank = (e) => {
+    this.setState({
+      changeBank: true,
+    })
+
+    console.log("changeBank: ", this.state.changeBank);
+  }
+
+
+
   render() {
+    const playerArr = this.state.gameState.players.map((player) => (
+      <div key={player}>
+      {player}
+      <Button onClick = {(e) => this.changeBank(e)} className="center-block" variant="contained" color="primary">Edit Bank</Button>
+      </div>
+    ));
+
     return (
+      <div>
+      {this.state.changeBank?
+        <div>
+        <Bank />
+        </div>
+        :
         <div>
         <h1> Main!!! </h1>
+        <h1> Turn {this.state.turn} </h1>
+        {playerArr}
+        </div>}
         </div>
       );
     }
